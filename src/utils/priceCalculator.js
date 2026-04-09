f743df83392886d79774a7165e0edfe1e747dafe
Ramit Roshann <ramitroshan777777@gmail.com>
2026-04-09 15:32:16 +0530

    feat: add price calculator utility

---
+++ src/utils/priceCalculator.js
@@ -0,0 +1,25 @@
+const calculatePrice = (vehicleType, entryTime, exitTime) => {
+  const diffMillsec = exitTime - entryTime;
+
+  let hours = Math.ceil(diffMillsec / (1000 * 60 * 60));
+
+  let amount = 0;
+
+  if (vehicleType === "bike") {
+    amount = 10 + (hours - 1) * 5;
+
+  } else if (vehicleType === "car") {
+    amount = 20 + (hours - 1) * 10;
+
+  } else if (vehicleType === "truck") {
+    
+    amount = 30 + (hours - 1) * 15;
+  }
+
+  return amount;
+};
+
+module.exports = calculatePrice;
+
+
+//calculating parking price based on vehicle type, entryTime and exitTime
