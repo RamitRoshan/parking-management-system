1d52bc873ce61d5f5e5392a5214ca9d8b0061b35
Ramit Roshann <ramitroshan777777@gmail.com>
2026-04-09 17:22:10 +0530

    feat: add morgan middleware for proper logging

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const morgan = require("morgan");
 const configureDB = require("./src/config/db");
 
-
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -13,24 +12,17 @@ configureDB();
 app.use(express.json());
 app.use(morgan("dev"));
 
+const slotRoutes = require("./src/routes/slot-routes");
+const ticketRoutes = require("./src/routes/ticket-routes");
 
-
-// Routes
-const slotRoutes = require("./src/routes/slot.routes");
-const ticketRoutes = require("./src/routes/ticket.routes");
-
-
-// Routes
 app.use("/api/slots", slotRoutes);
 app.use("/api/tickets", ticketRoutes);
 
-// Testing route
 app.get("/", (req, res) => {
-  res.send("Parking Management System API");
+  res.send("Parking Management System running ");
 });
-
 
 //start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
